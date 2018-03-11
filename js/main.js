66c481da6691d1f373ef3a0ba84a9ce3b90c161a
Guchiechan <36108529+Guchiechan@users.noreply.github.com>
2018-03-11 18:22:05 +0000

    Implemented Draggable link option

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 // Put your JavaScript code in the block below. You can remove line 3.
 //$(document).ready(function() {
-    alert("Your JavaScript file is working.")
-})
+//    alert("Your JavaScript file is working.")
+//})
+
+
+$( function() {
+  $( "#draggable" ).draggable();
+} );
